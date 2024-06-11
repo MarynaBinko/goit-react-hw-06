cb7711891be84a05f61284efa2cd8b7ac0e0533c
Maryna Binko <benyhorses@hotmail.com>
2024-06-11 10:02:03 +0100

    search box

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,2 +1,23 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { changeFilter, selectNameFilter } from '../../redux/filtersSlice';
+
+const SearchBox = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(selectNameFilter);
+
+  const handleChange = (event) => {
+    dispatch(changeFilter(event.target.value));
+  };
+
+  return (
+    <div>
+      <h2>Find contacts by name</h2>
+      <input type="text" value={filter} onChange={handleChange} />
+    </div>
+  );
+};
+
+export default SearchBox;
+
 
 

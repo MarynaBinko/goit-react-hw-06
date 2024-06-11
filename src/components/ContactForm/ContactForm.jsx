23233ba6c344dd344b552f2aa4cb99d3fe257f4d
Maryna Binko <benyhorses@hotmail.com>
2024-06-11 10:03:16 +0100

    contact form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,2 +1,46 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { nanoid } from 'nanoid';
+import * as Yup from 'yup';
+import { useDispatch } from 'react-redux';
+import { addContact } from '../../redux/contactsSlice';
+
+const ContactForm = () => {
+  const dispatch = useDispatch();
+
+  const initialValues = {
+    name: '',
+    number: ''
+  };
+
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().min(3, 'Too Short!').max(50, 'Too Long!').required('Required'),
+    number: Yup.string().min(3, 'Too Short!').max(50, 'Too Long!').required('Required')
+  });
+
+  const handleSubmit = (values, { resetForm }) => {
+    dispatch(addContact({ id: nanoid(), ...values }));
+    resetForm();
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={validationSchema}>
+      <Form>
+        <div>
+          <label>Name</label>
+          <Field type="text" name="name" />
+          <ErrorMessage name="name" component="div" />
+        </div>
+        <div>
+          <label>Number</label>
+          <Field type="text" name="number" />
+          <ErrorMessage name="number" component="div" />
+        </div>
+        <button type="submit">Add contact</button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default ContactForm;
 
 
